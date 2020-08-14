61f352a753a4af5fbfce5800b01a7a39517c0fab
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-08-14 08:54:37 -0700

    created actions, reducers folder and install dependencies

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+
+const store = createStore(smurfReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
